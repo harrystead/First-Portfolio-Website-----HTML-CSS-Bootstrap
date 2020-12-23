dcc466a9257c73aa42337048270ba77c46896c77
harrystead <69797257+harrystead@users.noreply.github.com>
2020-12-23 18:14:59 +0000

    Delete app-portfolio.js

--- app-portfolio.js
+++
@@ -1,7 +0,0 @@
-var headingSection = document.getElementsByClassName("nav-list");
-
-var newItemLast = document.createElement('li');
-var newTextLast = document.createTextNode('homosexuality');
-newItemLast.appendChild(newTextLast);
-List.appendChild(newItemLast);
-
