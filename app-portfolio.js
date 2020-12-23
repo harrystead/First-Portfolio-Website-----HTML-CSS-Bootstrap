5c6299ab09e4ff61f22bc26d266aa763056c9492
Harry Stead <harrystead9@gmail.com>
2020-12-23 18:07:16 +0000

    new html layout

---
+++ app-portfolio.js
@@ -0,0 +1,7 @@
+var headingSection = document.getElementsByClassName("nav-list");
+
+var newItemLast = document.createElement('li');
+var newTextLast = document.createTextNode('homosexuality');
+newItemLast.appendChild(newTextLast);
+List.appendChild(newItemLast);
+
